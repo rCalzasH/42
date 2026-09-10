0d387b0c0d40dd1d039dd86adceffe051c11cb58
rCalzasH <raulcalzashdez@gmail.com>
2026-09-10 11:08:08 +0200

    libft.h y MAKEFILE

--- libft/libft.h
+++ libft/libft.h
@@ -2,6 +2,27 @@
 # define LIBFT_H
 # include <stdlib.h>
 
-
+int ft_atoi(char *number);
+void *ft_bzero(void *mem, size_t n);
+void *calloc(size_t n, size_t size);
+int ft_isalpha(char c);
+int ft_isalum(char c);
+int ft_isascii(char c);
+int ft_isdigit(char c);
+int ft_isprint(char c);
+void *memchr(const void *s, int c, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+void    *memcpy(void *dest, const void *src, size_t n);
+void ft_memmove(void *dest, void *src, int size);
+void *ft_memset(void *mem, int x, size_t n);
+char *ft_strchr(const char *s, int c);
+char *ft_strdup(const char *s);
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+int ft_strlen(char *str);
+int ft_strncmp(char *s1, char *s2, size_t n);
+char *strrchr(char *s, char c);
+int ft_topupper(int param);
+int ft_topupper(int param);
 
 #endif
