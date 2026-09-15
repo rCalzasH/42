88e9c3c5a4ac653ccef6a91112f9f7bb75236b9c
rCalzasH <raulcalzashdez@gmail.com>
2026-09-15 16:17:55 +0200

    ft_putnbr_fd

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcalzas <rcalzas@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/15 15:59:46 by rcalzas           #+#    #+#             */
+/*   Updated: 2026/09/15 16:08:12 by rcalzas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+*@brief:writes an integer to a given filedescriptor
+*@param:the integer n, and the filedescriptor fd
+*@return:no return value
+*/
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	number;
+	char	c;
+
+	number = n;
+	if(number < 0)
+	{
+		number*=-1;
+		write(fd, "-", 1);
+	}
+	if(number > 9)
+	{
+		c = (number%10) - '0';
+		return(ft_putnbr_fd(n/10, fd);
+		write(fd, &c, 1);
+	}
+	else
+	{
+		c =  number -'0';
+		write(fd, &c, 1);
+		return;
+	}
+}
