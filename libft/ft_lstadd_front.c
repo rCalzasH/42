773543d8ae3aeeccf2b93b2575f9f7872573a627
rCalzasH <raulcalzashdez@gmail.com>
2026-09-16 17:13:55 +0200

    listas

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,13 +6,13 @@
 /*   By: rcalzas <rcalzas@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/16 10:23:42 by rcalzas           #+#    #+#             */
-/*   Updated: 2026/09/16 10:28:19 by rcalzas          ###   ########.fr       */
+/*   Updated: 2026/09/16 17:13:39 by rcalzas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 /*
-*@brief:
+*@brief:ads a new  node to the list given
 *@param:
 *return:
 */
--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,24 +6,29 @@
 /*   By: rcalzas <rcalzas@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/16 10:36:36 by rcalzas           #+#    #+#             */
-/*   Updated: 2026/09/16 10:43:01 by rcalzas          ###   ########.fr       */
+/*   Updated: 2026/09/16 15:30:44 by rcalzas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 /*
-*@brief
-*@param
-*@return
+*@brief:returns the last node of the list
+*@param: the a pointer to the list which last node que want to obtain
+*@return: NULL if wrong params or the last node of lst
 */
 t_list *ft_lstlast(t_list *lst)
 {
 	unsigned int	lst_s;
 	unsigned int	counter;
-	t_list			*cursor;
+	void			*cursor;
+	if (!lst)
+		return (NULL);
 	lst_s = ft_lstsize(lst);
 	cursor = lst->next;
-	while(counter++ < lst_s)
-		cursor = lst->next;
+	while(counter < lst_s)
+		{
+			cursor = lst->next;
+			counter++;
+		}
 	return (cursor);
 }
